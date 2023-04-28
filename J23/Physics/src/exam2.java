02f4522c4ade48d9c87a72299e370792d5157d4d
Winston <winstontsia@gmail.com>
2023-04-28 14:08:53 -0700

    updating prog

--- J23/Physics/src/exam2.java
+++ J23/Physics/src/exam2.java
@@ -44,35 +44,39 @@ public class exam2 {
 		double v6_2 = q6_0/(c_2 +c_3);
 		return v6_2*c_2*100; //in_the_figure_3 25 63 14 7
 	}
-	/*
-	public static double questionSeven() {
+	
+	public static double questionSeven(double c, double v) {
 	      // some calculations and logic
-		return //return val;
+		
+		return 3*c/5;
 	}
-	/*
-	public static double questionFour() {
+	
+	public static double questionEight(double v, double r_a, double r_b, double r) {
 	      // some calculations and logic
-		return //return val; //convert V to microV
+		return 0.5*8.84*Math.pow(v/(1/r_a-1/r_b)/Math.pow(r, 2), 2)/Math.pow(10, 6); //an_air_filled_sphere 1013 26.4 30 27.4
 	}
-	/*
-	public static double questionFive() {
+	
+	public static double questionNine(double phi, double i, double r, double bigL) {
 	      // some calculations and logic
-		return //return val;
+		
+		double bigR9 = phi*bigL/(Math.PI*Math.pow(r, 2));
+		return i*bigR9/10000; //a_wire_is_made 5.5 12.5 1 63
 	}
-	/*
-	public static double questionTwo() {
+	
+	public static double questionTen(double r_1, double r_2, double r_3) {
 	      // some calculations and logic
-		return //return val;
+		double r10_12 = r_1*r_2/(r_1 + r_2);
+		return r10_12 + r_3; //resistors_of_value 102 422 840
 	}
-	/*
-	public static double questionThree() {
+	
+	public static double questionEleven(double r, double c, double v_0, double t, double charge) {
 	      // some calculations and logic
-		return //return val;
+		return v_0/r*(1/charge)*1000; //an_r_resistor_init_pot 181 43 11 0 7
 	}
-	/*
-	public static double questionFour() {
+	
+	public static double questionTwelve(double resis, double capac, double pot_src, double t, double percent) {
 	      // some calculations and logic
-		return //return val; //convert V to microV
+		return 1; //
 	}
 	/*
 	public static double questionFive() {
@@ -109,7 +113,7 @@ public class exam2 {
 		System.out.println("type q to exit");
 		
 		while (checkerNum == true) {
-			System.out.println("Enter the Question keyword (two_elec_charges;a_charge_and_mass;a_particle_of_charge;a_cylindrical_capacitor;a_parallel_plate;in_the_figure_3;):");
+			System.out.println("Enter the Question keyword (two_elec_charges;a_charge_and_mass;a_particle_of_charge;a_cylindrical_capacitor;a_parallel_plate;in_the_figure_3;\nin_the_figure_all;an_air_filled_sphere;a_wire_is_made;resistors_of_value;an_r_resistor_init_pot;):");
 			
 			questionKey = scnr.next();
 			
@@ -167,50 +171,54 @@ public class exam2 {
 						double c6_3 = scnr.nextDouble();
 						double v6 = scnr.nextDouble();
 						System.out.println(questionSix(c6_1, c6_2, c6_3, v6));
-						break;/*
-					case "CASE 1":
-						System.out.println("CASE INSTRUCTIONS");
-						double volts = scnr.nextDouble();
-						double watts = scnr.nextDouble();
-						double currentPercent = scnr.nextDouble();
-						System.out.println(questionThree(volts, watts, currentPercent));
-						break;/*
-					case "CASE 1":
-						System.out.println("CASE INSTRUCTIONS");
-						double resivity = scnr.nextDouble();
-						double current = scnr.nextDouble();
-						double radius = scnr.nextDouble();
-						double length = scnr.nextDouble();
-						System.out.println(questionFour(resivity, current, radius, length));
-						break;/*
-					case "CASE 1":
-						System.out.println("CASE INSTRUCTIONS");
-						double resivity4 = scnr.nextDouble();
-						double currentDensity = scnr.nextDouble();
-						System.out.println(questionFive(resivity4, currentDensity));
 						break;
-						/*
-					case "battery_voltage":
-						System.out.println("CASE INSTRUCTIONS");
-						double watt = scnr.nextDouble();
-						double voltage = scnr.nextDouble();
-						double time = scnr.nextDouble();
-						System.out.println(questionTwo(watt, voltage, time));
-						break;/*
-					case "CASE 1":
-						System.out.println("CASE INSTRUCTIONS");
-						double volts = scnr.nextDouble();
-						double watts = scnr.nextDouble();
-						double currentPercent = scnr.nextDouble();
-						System.out.println(questionThree(volts, watts, currentPercent));
-						break;/*
-					case "CASE 1":
-						System.out.println("CASE INSTRUCTIONS");
-						double resivity = scnr.nextDouble();
-						double current = scnr.nextDouble();
-						double radius = scnr.nextDouble();
-						double length = scnr.nextDouble();
-						System.out.println(questionFour(resivity, current, radius, length));
+					case "in_the_figure_all":
+						System.out.println("(abs) Give double c, double v");
+						double c7 = scnr.nextDouble();
+						double v7 = scnr.nextDouble();
+						System.out.println(questionSeven(c7, v7));
+						break;
+					case "an_air_filled_sphere":
+						System.out.println("(abs) Give double v, double r_a, double r_b, double r");
+						double v8 = scnr.nextDouble();
+						double r8_a = scnr.nextDouble();
+						double r8_b = scnr.nextDouble();
+						double r8 = scnr.nextDouble();
+						System.out.println(questionEight(v8, r8_a, r8_b, r8));
+						break;
+					case "a_wire_is_made":
+						System.out.println("(abs) Give double phi, double i, double r, double bigL");
+						double phi9 = scnr.nextDouble();
+						double i9 = scnr.nextDouble();
+						double r9 = scnr.nextDouble();
+						double bigL9 = scnr.nextDouble();
+						System.out.println(questionNine(phi9, i9, r9, bigL9));
+						break;
+						
+					case "resistors_of_value":
+						System.out.println("(abs) Give double r_1, double r_2, double r_3");
+						double r10_1 = scnr.nextDouble();
+						double r10_2 = scnr.nextDouble();
+						double r10_3 = scnr.nextDouble();
+						System.out.println(questionTen(r10_1, r10_2, r10_3));
+						break;
+					case "an_r_resistor_init_pot":
+						System.out.println("(abs) give double r, double c, double v_0, double t, double charge (denominator)");
+						double r11 = scnr.nextDouble();
+						double c11 = scnr.nextDouble();
+						double v11_0 = scnr.nextDouble();
+						double t11 = scnr.nextDouble();
+						double charge11 = scnr.nextDouble();
+						System.out.println(questionEleven(r11, c11, v11_0, t11, charge11));
+						break;
+					case "an_r_resistor_pot_src":
+						System.out.println("(abs) give double resis, double capac, double pot_src, double t, double percent");
+						double resis12 = scnr.nextDouble();
+						double capac12 = scnr.nextDouble();
+						double pot_src12 = scnr.nextDouble();
+						double t12 = scnr.nextDouble();
+						double percent12 = scnr.nextDouble();
+						System.out.println(questionTwelve(resis12, capac12, pot_src12, t12, percent12));
 						break;/*
 					case "CASE 1":
 						System.out.println("CASE INSTRUCTIONS");
